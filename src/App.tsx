a2cb0362f1f5b9f5fcdf6f68c50fe6d209cab582
enkuria <kengeene@gmail.com>
2024-07-31 12:14:09 +0300

    refactor: add bottom content section

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import '@/assets/css/App.css'
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-gray-300 px-10">
-      <div className='h-screen'>
-        <h1>Analytics</h1>
+    <div className="h-screen w-screen bg-gray-300 p-10">
+      <h1>Analytics</h1>
+      <div className="grid grid-cols-3 gap-10 h-full">
+        <div className="bg-white p-5 rounded-lg">menu</div>
+        <div className="col-span-2 bg-white p-5 rounded-lg">content</div>
       </div>
     </div>
   );
