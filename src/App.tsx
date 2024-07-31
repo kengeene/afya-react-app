29a6c817b9586d8118b8a122738e5d67fa1aa31c
enkuria <kengeene@gmail.com>
2024-07-31 11:32:05 +0300

    feat: add shadcn for UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import '@/assets/css/App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <div className="h-screen w-screen bg-gray-100 px-10">
-      <div className="flex flex-col">Top Bar</div>
+      <div className="grid grid-cols-3">
+        <div>
+          <Button variant="secondary">Add Assesment</Button>
+        </div>
+        <div>
+          <Button variant="secondary">Secondary</Button>
+        </div>
+        <div>
+          <Button variant="secondary">Secondary</Button>
+        </div>
+      </div>
       <div>Bottom Section</div>
     </div>
   );
