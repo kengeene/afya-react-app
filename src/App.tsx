cc33000b6814742375762fdeca671435405393b1
enkuria <kengeene@gmail.com>
2024-07-31 20:18:11 +0300

    style: add margin to analytics header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainContent from '@/components/MainContent/index';
 function App() {
   return (
     <div className="h-full w-screen bg-gray-200 p-10">
-      <h1>Analytics</h1>
+      <div className='text-2xl my-10 font-bold'>Analytics</div>
       <div className="grid grid-cols-4 gap-10 h-full">
         <SideBar />
         <MainContent />
