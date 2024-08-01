68b9161dfaba28447d1821b752a21c4e57933962
enkuria <kengeene@gmail.com>
2024-08-01 12:35:04 +0300

    feat: add apollo client to fetch data

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,35 @@ import App from '@/App'
 import '@/assets/css/index.css'
 import TopBar from "@/components/TopBar";
 import { Folder, Activity, MessageSquareMore, SlidersVertical } from "lucide-react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
 const baseClasses = "my-5 :hover cursor-pointer";
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="grid grid-cols-12 bg-white">
-      <div className="flex flex-col justify-center items-center h-1/2">
-        <Folder className={baseClasses} />
-        <Activity className={baseClasses + " text-purple-600"} />
-        <MessageSquareMore className={baseClasses} />
-        <SlidersVertical className={baseClasses} />
+    <ApolloProvider client={client}>
+      <div className="grid grid-cols-12 bg-white">
+        <div className="flex flex-col justify-center items-center h-1/2">
+          <Folder className={baseClasses} />
+          <Activity className={baseClasses + " text-purple-600"} />
+          <MessageSquareMore className={baseClasses} />
+          <SlidersVertical className={baseClasses} />
+        </div>
+        <div className="col-span-11">
+          <TopBar />
+          <App />
+        </div>
       </div>
-      <div className="col-span-11">
-        <TopBar />
-        <App />
-      </div>
-    </div>
+    </ApolloProvider>
   </React.StrictMode>
 );
